86ca184e469e774faa5a40c42dd0c3ed8c462af3
Elcand <tjandraraharja99@gmail.com>
2025-07-01 13:09:46 +0700

    set up google

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import vue3GoogleLogin from "vue3-google-login";
+const app = createApp(App);
 
-createApp(App).mount('#app')
+app.use(vue3GoogleLogin, {
+  clientId:
+    "508789945468-f1ak651ja7mpf0ht2qn8cahu4mddue3v.apps.googleusercontent.com",
+});
+
+app.mount("#app");
